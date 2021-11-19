e54f0c7e1dfe8e4c66a8fc94aa2eb3311c5065ea
Muhammad Hafidz <halfpheez92@gmail.com>
2021-11-19 18:49:03 +0800

    updated to calculate the factorial of N.

--- section01/section01Ans.js
+++ section01/section01Ans.js
@@ -1,3 +1,14 @@
 "use strict";
-console.log("answer code goes here.");
-console.log("solution goes here.");
+console.log("Solution for Section 1 goes here.");
+function factorialFunction(var1) {
+    let resultFactorial = 0;
+    if (var1 === 0) {
+        return 1;
+    }
+    else {
+        resultFactorial = var1 * factorialFunction(var1 - 1);
+    }
+    return resultFactorial;
+}
+var userInput = Number(prompt('Please enter N data number.'));
+console.log(`The result of ${userInput} => `, factorialFunction(userInput));
--- section01/section01Ans.ts
+++ section01/section01Ans.ts
@@ -1,2 +1,15 @@
-console.log("answer code goes here.");
-console.log("solution goes here.");
+console.log("Solution for Section 1 goes here.");
+
+function factorialFunction(var1: number): number {
+    let resultFactorial: number = 0;
+    
+    if (var1 === 0) {
+        return 1;
+    } else {
+        resultFactorial = var1*factorialFunction(var1-1);
+    }
+    return resultFactorial;
+}
+
+var userInput = Number(prompt('Please enter N data number.'));
+console.log(`The result of ${userInput} => `, factorialFunction(userInput));
