00cb761ad73bad3a629f1602ec7a2b1998801b17
Muhammad Hafidz <halfpheez92@gmail.com>
2021-11-19 22:46:14 +0800

    added solution for section 1 for the sum of digits of factorial.

--- section01/section01Ans.js
+++ section01/section01Ans.js
@@ -1,14 +1,27 @@
 "use strict";
 console.log("Solution for Section 1 goes here.");
 function factorialFunction(var1) {
-    let resultFactorial = 0;
-    if (var1 === 0) {
-        return 1;
+    let varFactorial = 1;
+    // get the result of factorial.
+    for (let index = 2; index <= var1; index++) {
+        varFactorial = varFactorial * index;
     }
-    else {
-        resultFactorial = var1 * factorialFunction(var1 - 1);
-    }
-    return resultFactorial;
+    return varFactorial;
 }
-var userInput = Number(prompt('Please enter N data number.'));
-console.log(`The result of ${userInput} => `, factorialFunction(userInput));
+function sumOfDigits(numVar) {
+    // convert to string.
+    const strVar = `${numVar}`;
+    let sumDigits = 0;
+    // sum of the digits in array of string.
+    for (let index = 0; index < strVar.length; index++) {
+        // the sums of converted integers.
+        sumDigits += +(strVar[index]);
+    }
+    return sumDigits;
+}
+function sumDigitsFactorial(digitNum) {
+    return sumOfDigits(factorialFunction(digitNum));
+}
+var userInput = Number(prompt("Please enter N data number."));
+console.log(`The result of factorial of ${userInput}! => `, factorialFunction(userInput));
+console.log(`The sum of the digits in the number ${userInput}! => `, sumDigitsFactorial(userInput));
--- section01/section01Ans.ts
+++ section01/section01Ans.ts
@@ -1,15 +1,34 @@
 console.log("Solution for Section 1 goes here.");
 
 function factorialFunction(var1: number): number {
-    let resultFactorial: number = 0;
-    
-    if (var1 === 0) {
-        return 1;
-    } else {
-        resultFactorial = var1*factorialFunction(var1-1);
-    }
-    return resultFactorial;
+  let varFactorial: number = 1;
+
+  // get the result of factorial.
+  for (let index = 2; index<=var1; index++) {
+    varFactorial = varFactorial * index;
+  }
+
+  return varFactorial;
 }
 
-var userInput = Number(prompt('Please enter N data number.'));
-console.log(`The result of ${userInput} => `, factorialFunction(userInput));
+function sumOfDigits(numVar: number): number {
+    // convert to string.
+    const strVar = `${numVar}`;
+    let sumDigits: number = 0;
+
+    // sum of the digits in array of string.
+    for (let index = 0; index < strVar.length; index++) {
+        // the sums of converted integers.
+        sumDigits += +(strVar[index]);
+    }
+
+    return sumDigits;
+}
+
+function sumDigitsFactorial(digitNum: number): number {
+    return sumOfDigits(factorialFunction(digitNum));
+}
+
+var userInput = Number(prompt("Please enter N data number."));
+console.log(`The result of factorial of ${userInput}! => `, factorialFunction(userInput));
+console.log(`The sum of the digits in the number ${userInput}! => `, sumDigitsFactorial(userInput));
