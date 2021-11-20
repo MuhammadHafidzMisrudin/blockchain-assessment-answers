e4b39afe29bb4517ac74be58c5443b75d7f0b2cc
Muhammad Hafidz <halfpheez92@gmail.com>
2021-11-21 02:16:32 +0800

    added class solution for question 1 of section2.

--- section02/section02Ans01.js
+++ section02/section02Ans01.js
@@ -1,3 +1,60 @@
 "use strict";
 console.log("Solution 1 for Section 2 goes here.");
-console.log("Solution 2 for Section 2 goes here.");
+class Account {
+    // constructor. 
+    constructor(id, name, balance) {
+        // function to check if the amount is positive.
+        this.positiveAmount = (amount) => {
+            let isPositive = amount > 0;
+            if (!isPositive) {
+                console.log("amount must be positive.");
+                return false;
+            }
+            return true;
+        };
+        // function to check if the amount is positive and within the balance.
+        this.allowWithdraw = (amount) => {
+            if (!this.positiveAmount(amount)) {
+                return false;
+            }
+            let withdrawAllowed = this.balance - amount >= 0;
+            if (!withdrawAllowed) {
+                console.log("insuffient funds.");
+                return false;
+            }
+            return true;
+        };
+        // function to set deposit.
+        this.deposit = (amount) => {
+            let newBalance = 0;
+            if (this.positiveAmount(amount)) {
+                newBalance = this.balance + amount;
+            }
+            this.balance = newBalance;
+            console.log(`amount of deposit = ${amount}`);
+            return this.balance;
+        };
+        this.withdraw = (amount) => {
+            let newBalance = 0;
+            if (this.allowWithdraw(amount)) {
+                newBalance = this.balance - amount;
+            }
+            this.balance = newBalance;
+            console.log(`amount of withdrawal = ${amount}`);
+            return this.balance;
+        };
+        this.displayAccountStatement = () => {
+            console.log(`${this.name}'s account. Balance: ${this.balance}`);
+        };
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+}
+// Demo the instances of the Account class.
+const accountA = new Account(1, "Hafidz", 3500);
+console.log(`${accountA.name}'s current balance = ${accountA.balance}'`);
+accountA.deposit(500);
+accountA.displayAccountStatement();
+accountA.withdraw(1000);
+accountA.displayAccountStatement();
--- section02/section02Ans01.ts
+++ section02/section02Ans01.ts
@@ -1,2 +1,72 @@
 console.log("Solution 1 for Section 2 goes here.");
-console.log("Solution 2 for Section 2 goes here.");
+
+class Account {
+    // initialise fields.
+    id: number;
+    name: string;
+    balance: number;
+
+    // constructor. 
+    constructor(id: number, name: string, balance: number) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    // function to check if the amount is positive.
+    positiveAmount = (amount: number): boolean => {
+        let isPositive = amount > 0;
+        if (!isPositive) {
+            console.log("amount must be positive.");
+            return false;
+        }
+        return true;
+    }
+
+    // function to check if the amount is positive and within the balance.
+    allowWithdraw = (amount: number): boolean => {
+        if (!this.positiveAmount(amount)) {
+            return false;
+        }
+
+        let withdrawAllowed = this.balance - amount >= 0;
+        if (!withdrawAllowed) {
+            console.log("insuffient funds.");
+            return false;
+        }
+        return true; 
+    }
+
+    // function to set deposit.
+    deposit = (amount: number): number => {
+        let newBalance: number = 0;
+        if (this.positiveAmount(amount)) {
+            newBalance = this.balance + amount;
+        }
+        this.balance = newBalance
+        console.log(`amount of deposit = ${amount}`);
+        return this.balance;
+    }
+
+    withdraw = (amount: number): number => {
+        let newBalance: number = 0;
+        if (this.allowWithdraw(amount)) {
+            newBalance = this.balance - amount;
+        }
+        this.balance = newBalance;
+        console.log(`amount of withdrawal = ${amount}`);
+        return this.balance;
+    }
+
+    displayAccountStatement = () => {
+        console.log(`${this.name}'s account. Balance: ${this.balance}`);
+    }
+}
+
+// Demo the instances of the Account class.
+const accountA = new Account(1, "Hafidz", 3500);
+console.log(`${accountA.name}'s current balance = ${accountA.balance}'`)
+accountA.deposit(500);
+accountA.displayAccountStatement();
+accountA.withdraw(1000);
+accountA.displayAccountStatement();
